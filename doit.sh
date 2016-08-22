9e52f7cda89033801662620ed27381d8cc85e9c7
Jeff Johnson <jbj@rpm5.org>
2016-08-22 17:34:35 -0400

    - rebuild on El Capitan.

--- doit.sh
+++ doit.sh
@@ -4,7 +4,7 @@ rm -rf build
 
 mkdir build
 cd build && cmake \
-        -DCMAKE_C_FLAGS:STRING="-I/opt/local/include -I/opt/local/include/db62" \
+        -DCMAKE_C_FLAGS:STRING="-I/opt/local/include -I/opt/local/include/db62"\
         -DCMAKE_C_FLAGS_RELEASE:STRING="-DNDEBUG" \
         -DCMAKE_CXX_FLAGS_RELEASE:STRING="-DNDEBUG" \
         -DCMAKE_Fortran_FLAGS_RELEASE:STRING="-DNDEBUG" \
@@ -15,21 +15,27 @@ cd build && cmake \
         -DSYSCONF_INSTALL_DIR:PATH=/opt/local/etc \
         -DSHARE_INSTALL_PREFIX:PATH=/opt/local/share \
         -DBUILD_SHARED_LIBS:BOOL=ON \
-    -DRPM5=1                                    \
-    -DMACOSX=1                                    \
-    -DENABLE_RPMDB=ON                             \
-    -DENABLE_RPMDB_BYRPMHEADER=ON                 \
-    -DENABLE_RPMMD=ON                             \
-    -DENABLE_COMPS=ON              \
-    -DENABLE_APPDATA=ON          \
-    -DUSE_VENDORDIRS=ON                           \
-    -DENABLE_LZMA_COMPRESSION=ON                  \
-    -DENABLE_BZIP2_COMPRESSION=ON                 \
-    -DENABLE_DEBIAN=ON       \
-    -DENABLE_ARCHREPO=ON       \
-    -DENABLE_HELIXREPO=ON     \
+    -DRPM5=1 \
+    -DMACOSX=1 \
+    -DENABLE_RPMDB=ON \
+    -DENABLE_RPMDB_BYRPMHEADER=ON \
+    -DENABLE_RPMMD=ON \
+    -DENABLE_COMPS=ON \
+    -DENABLE_APPDATA=ON \
+    -DUSE_VENDORDIRS=ON \
+    -DENABLE_LZMA_COMPRESSION=ON \
+    -DENABLE_BZIP2_COMPRESSION=ON \
+    -DENABLE_DEBIAN=ON \
+    -DENABLE_ARCHREPO=ON \
+    -DENABLE_HELIXREPO=ON \
     -DMULTI_SYMANTICS=ON \
     -DENABLE_COMPLEX_DEPS=1 \
     -DENABLE_PERL=OFF \
     -DENABLE_RUBY=OFF \
 	../
+
+cd build
+make
+make test
+
+sudo make install
