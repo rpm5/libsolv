512766c0babe17ebcf9bc04512a551e68e5939ee
Jeff Johnson <n3npq@mac.com>
2017-03-31 16:18:47 -0400

    - add -fsanitize=address (see https://github.com/arsenm/sanitizers-cmake).

--- doit.sh
+++ doit.sh
@@ -6,15 +6,23 @@ mkdir build
 cd build
 
 cmake \
-        -DCMAKE_C_FLAGS:STRING="-I/opt/local/include -I/opt/local/include/db62"\
+	-DCMAKE_BUILD_TYPE:STRING=DEBUG \
+        -DCMAKE_C_FLAGS:STRING="-I/opt/local/include -I/opt/local/rpm -I/usr/include/db62"\
         -DCMAKE_C_FLAGS_RELEASE:STRING="-DNDEBUG" \
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
         -DCMAKE_INSTALL_PREFIX:PATH=/opt/local \
+	-DCMAKE_INSTALL_RPATH:PATH=/opt/local/lib \
         -DINCLUDE_INSTALL_DIR:PATH=/opt/local/include \
         -DLIB_INSTALL_DIR:PATH=/opt/local/lib \
+	-DSYSCONF_INSTALL_DIR:PATH=/opt/local/etc \
         -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DSANITIZE_ADDRESS:BOOL=ON \
+    -DSANITIZE_MEMORY:BOOL=OFF \
+    -DSANITIZE_THREAD:BOOL=OFF \
+    -DSANITIZE_UNDEFINED:BOOL=OFF \
     -DRPM5=1 \
-    -DMACOSX=1 \
+    -DMACOSX=0 \
+    -DFEDORA=1 \
     -DENABLE_RPMDB=ON \
     -DENABLE_RPMDB_BYRPMHEADER=ON \
     -DENABLE_RPMMD=ON \
@@ -30,8 +38,11 @@ cmake \
     -DENABLE_COMPLEX_DEPS=1 \
     -DENABLE_PERL=OFF \
     -DENABLE_RUBY=OFF \
+    -DENABLE_PYTHON=OFF \
+    -DPythonLibs_FIND_VERSION=2       \
+    -DPythonLibs_FIND_VERSION_MAJOR=2 \
 	../
 
-make && make test && sudo make install
+make -O -j8 && make ARGS="-VV --debug" test && sudo make install
 
-otool -L /opt/local/lib/libsolv.0.dylib
+#otool -L /opt/local/lib/libsolv.0.dylib
